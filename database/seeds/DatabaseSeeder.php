a2f7598811deb8bb8195d383059c4b5cdc193049
thettun13 <thettun1741997@gmail.com>
2021-11-22 18:10:36 +0630

    add laravel-cors

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        $this->call(BookSeeder::class);
+        $this->call(BookSeeder::class, 10);
 
     }
 }
