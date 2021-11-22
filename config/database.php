97fe9b7ca7cb8218758c31ae2b41cf4ae3000286
thettun13 <thettun1741997@gmail.com>
2021-11-22 11:29:27 +0630

    add db

--- config/database.php
+++ config/database.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Str;
 
+
+$DATABASE_URL=parse_url('postgres://ecukyyxiwcoaft:d9bd37458d65c70c7479fbe958574cda7673b6710f12d925820398e39d59bd9b@ec2-34-236-34-103.compute-1.amazonaws.com:5432/da7cs4n9bej3fd');
+
 return [
+
 
     /*
     |--------------------------------------------------------------------------
@@ -15,7 +19,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +69,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            // 'url' => env('DATABASE_URL'),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
